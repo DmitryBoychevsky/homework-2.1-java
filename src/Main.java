a08a24b9b078bd02c386285cd64b51f3d28d3e19
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-03-02 22:35:16 +0300

    application

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3000;
+        int rublesPerMile = 20;
+        int bonus = ticketPrice / rublesPerMile;
+        System.out.println(bonus);
+
+    }
+
+}
+
